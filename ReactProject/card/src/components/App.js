e89ec8cb7acedea981aa765b15f54a86ecb506b4
shaheen <shaheen.cse20@gmail.com>
2021-01-21 17:10:50 +0600

    structure folder

---
+++ ReactProject/card/src/components/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+function test() {
+
+  
+  console.log("test e achi");
+}
+
+test();
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>
+        <span>EMOJI jano, send koro, khusi thako</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
